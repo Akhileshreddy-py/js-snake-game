23ed8a13808c76a875ca6daf2b4b64c48a5b2394
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-06-27 17:46:48 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import './style.css';
 
 // Write Javascript code!
 const cvs = document.getElementById('canvas');
+var x=0;
 var ctx=cvs.getContext("2d");
+function move()
+{
+ctx.fillStyle="black";
+ctx.fillRect(x,9,10,15);
+x++;
+}
+
+setInterval(move,100)
