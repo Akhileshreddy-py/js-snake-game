119dff3772d324461defae1df08e8ee5df57d740
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-06-27 17:41:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-nhyv18
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-nhyv18)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const cvs = document.getElementById('canvas');
+var ctx=cvs.getContext("2d");
