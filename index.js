0fd9459bcf5ed030af658adcb2375a1cff1b5445
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-06-27 19:16:52 +0530

    3 files modified

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import './style.css';
 
 // Write Javascript code!
 const cvs = document.getElementById('canvas');
-var x=0;
-var ctx=cvs.getContext("2d");
-function move()
-{
-ctx.fillStyle="black";
-ctx.fillRect(x,9,10,15);
-x++;
+var x = 0;
+var ctx = cvs.getContext('2d');
+function drawXSnake(x,y) {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(x,0,10,10);
+  ctx.fillStyle="black";
+  ctx.stroke();
 }
+drawXSnake(10,10);
+drawXSnake(20,10);
 
-setInterval(move,100)
+
+
+//setInterval(move,100)
